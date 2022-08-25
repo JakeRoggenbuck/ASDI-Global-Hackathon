da4365f55386a0005013cc685d31a0824796b953
terry tong <dooshtheboss1@gmail.com>
2022-08-25 10:13:03 +0800

    Done

--- Backend/Rainpull.py
+++ Backend/Rainpull.py
@@ -5,13 +5,9 @@ from io import BytesIO
 import gzip
 import numpy as np
 
-
-
 from botocore.handlers import disable_signing
 s3_resource = boto3.resource('s3')
 s3_resource.meta.client.meta.events.register('choose-signer.s3.*', disable_signing)
-
-
 
 def collect_data():
     year = "2020"
@@ -51,16 +47,16 @@ long_requested = 17.33705959
 def read_data():
     month = "01"
     year = "2020"
-    
+
     for i in range(1,2): #still downloads all?
         day = str(i)
-        
+
         if (i < 10):
             day = "0" + day
-            
+
         date_file=f"2020{month}{day}"
         path = f"africa-rainfall/{date_file}.csv"
-        
+
         temp_dataframe = pd.read_csv(path)
         retrieve_data(temp_dataframe, lat_requested, long_requested)
 
@@ -69,22 +65,18 @@ def retrieve_data(df, latitude, longitude):
     # Both are now validated! Next, correct lat. and long. to begin at zero
     lat_corrected = 65 + latitude
     long_corrected = longitude + 180
-    
+
     # Convert coordinates into indexes (reminder - latitude is y, longitude is x)
     # TODO - Look into proper rounding (up/down)
     
     lat_index = round((4799.0 / 130) * lat_corrected - 923) 
     long_index = round((10019.0 / 360) * long_corrected - 4453) +1
-    
-    print("LAT INDEX = " + str(round(lat_index)))
-    print("LONG INDEX = " + str(round(long_index)))
-    
-    print(df.iloc[lat_index].iloc[long_index]) 
+    return 
 
 #in the dataframe, the labels are not included so we need to subtract one from horz and two from vert
-#might need to account for the csv printing the headers label when comparing to the actual 2d array. Or maybe because csv start at 1 and array start at 0. 
-# TODO , check this against the actual array and see if we are printing the right thing. 
-# TODO, check the conversion of the index to distance, between every margin of +- 0.5 of that index, refer to it - wait, don't need to worry about this because 'round' does it for us. Probably just something interesting to write about. 
-# TODO, take average of rainfall after verifying this is true. 
+#might need to account for the csv printing the headers label when comparing to the actual 2d array. Or maybe because csv start at 1 and array start at 0.
+# TODO , check this against the actual array and see if we are printing the right thing.
+# TODO, check the conversion of the index to distance, between every margin of +- 0.5 of that index, refer to it - wait, don't need to worry about this because 'round' does it for us. Probably just something interesting to write about.
+# TODO, take average of rainfall after verifying this is true.
 
 read_data()
---
+++ README.md
@@ -0,0 +1,28 @@
+![Plant Here Logo](https://raw.githubusercontent.com/GeorgeBerdovskiy/plant-here/main/static/Logo.png)
+# Plant Here!
+
+## Frontend (React)
+Frontend written in React.
+
+## Backend (FastAPI)
+Backend written in FastAPI.
+
+![image](https://user-images.githubusercontent.com/35516367/186083075-c959217d-e9d5-4f26-92f3-bf266d051ef0.png)
+
+#### Requirements
+1. Install the requirements.
+```
+pip install -r requirements.txt
+```
+
+2. Run the server
+```
+python3 main.py
+```
+and you should see this.
+```
+INFO:     Started server process [18369]
+INFO:     Waiting for application startup.
+INFO:     Application startup complete.
+INFO:     Uvicorn running on http://0.0.0.0:8000 (Press CTRL+C to quit)
+```
