4f8e1b43a8c49b2afabeda9950e2dbbeb9b4fca8
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-08-30 10:25:55 -0700

    Before redo

--- machine-learning/main.py
+++ machine-learning/main.py
@@ -1,1 +1,32 @@
 import pandas as pd
+from tensorflow import keras
+from tensorflow.keras import layers
+from datetime import datetime
+import os
+
+
+model = keras.Sequential(
+    [
+        keras.Input(shape=(1,)),
+        layers.Dense(32, activation="relu", name="layer1"),
+        layers.Dense(32, activation="relu", name="layer2"),
+        layers.Dense(, name="layer3"),
+    ]
+)
+
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+
+
+for l in os.listdir("../data/csvs/"):
+    df = pd.read_csv("../data/csvs/" + l)
+
+    # fill nan with zeros
+    df = df.fillna(0)
+
+    date = l.split(".")[0]
+    date = date.split("_")[-1]
+    date = date[:4] + "-" + date[4:6] + "-" + date[6:8]
+    date = datetime.fromisoformat(date)
+    days = (datetime.strptime('22-02-2026', '%d-%m-%Y') - date).days
+
+    model.train_on_batch((days,), df)
